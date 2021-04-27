03ef00440c2d188e2036998c6cb6b8e23a5c8f57
Michaela Bickish <mmbickish@gmail.com>
2021-04-26 21:48:15 -0600

    Input created

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,40 +1,76 @@
 <template>
   <div class="home container flex-grow-1 d-flex flex-column align-items-center justify-content-center">
     <div class="row" v-if="state.activeProfile">
-      <div class="col-md-12">
-        <img :src="state.activeProfile.picture" class="rounded-circle mb-0 p-4 active-profile-photo" alt="">
-        <h2>{{ state.activeProfile.name }}</h2>
-        <p class="text-muted">
-          {{ state.activeProfile.class || 'Spring 2021' }}
-        </p>
-        <hr>
-        <PostComponent v-for="post in state.posts" :key="post.id" :post="post" />
-        <OlderNewer />
+      <div class="col-md-12 text-center">
+        <div>
+          <img :src="state.activeProfile.picture" class="rounded-circle mb-0 p-4 active-profile-photo" alt="">
+          <p class="font-weight-bold">
+            {{ state.activeProfile.name }}
+          </p>
+          <p class="text-muted">
+            {{ state.activeProfile.class || 'Spring 2021' }}
+          </p>
+          <hr>
+        </div>
+
+        <div class="ProfilePage card shadow">
+          <p class="font-weight-bold mb-0 text-info">
+            New Post
+          </p>
+          <div class="card-body p-2">
+            <form @submit.prevent="createPost" v-if="state.user.isAuthenticated && state.account.id === route.params.id">
+              <div class="form-group">
+                <input type="text"
+                       class="form-control mb-1"
+                       name="comment"
+                       id="comment"
+                       placeholder="What's On Your Mind?"
+                       v-model="state.newPost.body"
+                       required
+                >
+                <input type="text"
+                       class="form-control"
+                       name="imgUrl"
+                       id="imgUrl"
+                       placeholder="Photo URL Here..."
+                       v-model="state.newPost.imgUrl"
+                >
+              </div>
+              <button type="submit" class="btn btn-outline-info m-0">
+                +
+              </button>
+            </form>
+          </div>
+
+          <PostComponent v-for="post in state.posts" :key="post.id" :post="post" />
+          <OlderNewer />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { computed, reactive, watchEffect } from 'vue'
+import { computed, onMounted, reactive } from 'vue'
 import { postsService } from '../services/PostsService'
 import { AppState } from '../AppState'
 import { useRoute } from 'vue-router'
 import { accountService } from '../services/AccountService'
 import { profileService } from '../services/ProfileService'
+import Notification from '../utils/Notification'
 export default {
   name: 'ProfilePage',
   setup() {
     const route = useRoute()
     const state = reactive({
-      // newPost: {},
+      newPost: {},
       activeProfile: computed(() => AppState.activeProfile),
       posts: computed(() => AppState.profilePosts),
       account: computed(() => AppState.account),
       user: computed(() => AppState.user)
     })
-    //  watchEffect
-    watchEffect(async() => {
+    //  watchEffect- gave a lot of 400 bad requests on home page.
+    onMounted(async() => {
       try {
         await accountService.getProfile(route.params.id)
         await profileService.getActiveProfile(route.params.id)
